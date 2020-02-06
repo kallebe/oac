e655a2b3b57695d9d23bfa22c5e6de42e99c6873
Rayxan <48591119+Rayxan@users.noreply.github.com>
2020-02-06 18:15:44 -0300

    Update menu.s

--- Projeto_oac/menu.s
+++ Projeto_oac/menu.s
@@ -1,48 +1,20 @@
-
-
 .data
 .include "MENU5.data"
-.include "isc.data"
-
-
 
 .text
-
-# Preenche a tela de vermelho
-	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
-	li t2,0xFF012C00	# endereco final 
-	li t3,0x07070707	# cor vermelho|vermelho|vermelhor|vermelho
-LOOP: 	beq t1,t2,FORA		# Se for o último endereço então sai do loop
-	sw t3,0(t1)		# escreve a word na memória VGA
-	addi t1,t1,4		# soma 4 ao endereço
-	j LOOP			# volta a verificar
-
-
 # Carrega a imagem1
 FORA:	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
 	li t2,0xFF012C00	# endereco final 
-	la s1,MENU5		# endereço dos dados da tela na memoria
-	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
-LOOP1: 	beq t1,t2,FORA1		# Se for o último endereço então sai do loop
+	la s1,MENU5		# endereÃ§o dos dados da tela na memoria
+	addi s1,s1,8		# primeiro pixels depois das informaÃ§Ãµes de nlin ncol
+LOOP1: 	beq t1,t2,FIM		# Se for o Ãºltimo endereÃ§o entÃ£o sai do loop
 	lw t3,0(s1)		# le um conjunto de 4 pixels : word
-	sw t3,0(t1)		# escreve a word na memória VGA
-	addi t1,t1,4		# soma 4 ao endereço
+	sw t3,0(t1)		# escreve a word na memÃ³ria VGA
+	addi t1,t1,4		# soma 4 ao endereÃ§o
 	addi s1,s1,4
 	j LOOP1			# volta a verificar
-
-
-# Carrega a imagem2
-FORA1:	li t1,0xFF100000	# endereco inicial da Memoria VGA - Frame 1
-	li t2,0xFF112C00	# endereco final 
-	la s1,isc		# endereço dos dados da tela na memoria
-	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
-LOOP2: 	beq t1,t2,FIM		# Se for o último endereço então sai do loop
-	lw t3,0(s1)		# le um conjunto de 4 pixels : word
-	sw t3,0(t1)		# escreve a word na memória VGA
-	addi t1,t1,4		# soma 4 ao endereço
-	addi s1,s1,4
-	j LOOP2			# volta a verificar
-	
+		
 # devolve o controle ao sistema operacional
 FIM:	li a7,10		# syscall de exit
 	ecall
+
